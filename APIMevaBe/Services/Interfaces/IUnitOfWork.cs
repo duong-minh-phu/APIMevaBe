dd41147cdeb52ee2eb9583f1c496b8db4cb215c7
duong-minh-phu <134716556+duong-minh-phu@users.noreply.github.com>
2024-07-23 15:54:28 +0700

    Services

---
+++ APIMevaBe/Services/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿
+using Repo.Models;
+using Services.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public GenericRepository<Blog> BlogRepository { get; }
+        public GenericRepository<BlogCategory> BlogCategoryRepository { get; }
+        public GenericRepository<Cart> CartRepository { get; }
+        public GenericRepository<CartItem> CartItemRepository { get; }
+        public GenericRepository<Feedback> FeedbackRepository { get; }
+        public GenericRepository<OrderDetail> OrderDetailRepository { get; }
+        public GenericRepository<OrderItem> OrderItemRepository { get; }
+        public GenericRepository<Payment> PaymentRepository { get; }
+        public GenericRepository<Product> ProductRepository { get; }
+        public GenericRepository<ProductCategory> ProductCategoryRepository { get; }
+        public GenericRepository<ProductSubImage> ProductSubImageRepository { get; }
+        public GenericRepository<User> UserRepository { get; }
+        public GenericRepository<UserAddress> UserAddressRepository { get; }
+        public void Save();
+    }
+}
