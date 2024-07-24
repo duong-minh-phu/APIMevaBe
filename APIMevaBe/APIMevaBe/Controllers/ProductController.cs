2aa5ae550e9daf7f6b176cb2c221b3f8aadb424f
duong-minh-phu <134716556+duong-minh-phu@users.noreply.github.com>
2024-07-24 23:30:08 +0700

    fullproduct

--- APIMevaBe/APIMevaBe/Controllers/ProductController.cs
+++ APIMevaBe/APIMevaBe/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PRN221_MeVaBe_Repo.Models;
 using Services.Interfaces;
 using Services.Repositories;
 
@@ -22,27 +23,67 @@ namespace APIMevaBe.Controllers
 
         // GET api/<ProductController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult GetEquipmentById(string id)
         {
-            return "value";
+            var products = _repository.ProductRepository.GetByID(id);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (products != null)
+            {
+                return Ok(products);
+            }
+            return NotFound();
         }
 
         // POST api/<ProductController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Product Product)
         {
+            _repository.ProductRepository.Insert(Product);
+            _repository.Save();
+            return Ok("Create product success");
         }
 
         // PUT api/<ProductController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] Product product)
         {
+            if (_repository.ProductRepository.GetByID(id) != null)
+            {
+                var _product = _repository.ProductRepository.GetByID(id);
+                if (product.ProductName != null)
+                {
+                    _product.ProductName = product.ProductName;
+                }
+                if (product.Price != null)
+                {
+                    _product.Price = product.Price;
+                }
+                if (product.Description != null)
+                {
+                    _product.Description = product.Description;
+                }
+
+                _repository.ProductRepository.Update(_product);
+                _repository.Save();
+                return Ok("Update product success");
+            }
+            return NotFound();
         }
 
         // DELETE api/<ProductController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            if (_repository.ProductRepository.GetByID(id) != null)
+            {
+                _repository.ProductRepository.Delete(id);
+                _repository.Save();
+                return Ok("delete thanh cong");
+            }
+            return NotFound();
         }
     }
 }
